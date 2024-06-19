750f98f77d98d3bee66a936322bc5576bf0115a7
thicco-catto <cristobal.martin01@gmail.com>
2024-06-19 17:31:10 +0200

    chore: Disabled info compile option

--- src/app/[surveyId]/consent/page.tsx
+++ src/app/[surveyId]/consent/page.tsx
@@ -42,17 +42,21 @@ function AskConsent() {
                     <main>
                         <h2>¿Permitir que se recopilen las respuestas?</h2>
                         <p>
-                        Antes de comenzar la encuesta, nos gustaría pedir su consentimiento
-                        para recopilar y utilizar sus respuestas. Si acepta, le haremos algunas
-                        preguntas personales antes de comenzar y utilizaremos sus datos y
-                        respuestas para mejorar la calidad de la encuesta y la precisión de los resultados.
-                        La participación es voluntaria y sus respuestas serán tratadas con confidencialidad.
+                            Antes de comenzar la encuesta, nos gustaría pedir su consentimiento
+                            para recopilar y utilizar sus respuestas. Si acepta, le haremos algunas
+                            preguntas personales antes de comenzar y utilizaremos sus datos y
+                            respuestas para mejorar la calidad de la encuesta y la precisión de los resultados.
+                            La participación es voluntaria y sus respuestas serán tratadas con confidencialidad.
+                        </p>
+
+                        <p>
+                            Por el momento la opción de recopilar datos no está disponible.
                         </p>
 
                         <Container className="yesno-container">
                             <Row>
                                 <Col>
-                                    <Button className="yesno-btn me-4" onClick={OnClickYes} variant="success">Sí</Button>
+                                    <Button className="yesno-btn me-4" onClick={OnClickYes} disabled variant="secondary">Sí</Button>
                                 </Col>
 
                                 <Col>
