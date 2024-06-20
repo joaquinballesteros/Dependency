9ba19e431b1ee81b46d59127cf99dc1de8ccfe9d
thicco-catto <cristobal.martin01@gmail.com>
2024-06-20 21:59:54 +0200

    feat: Updated consent page again

--- src/app/[surveyId]/consent/page.tsx
+++ src/app/[surveyId]/consent/page.tsx
@@ -22,9 +22,9 @@ function AskConsent() {
         }
     }, [surveyId]);
 
-    function OnClickYes() {
-        window.location.href = `/${surveyId}/selectProfile`;
-    }
+    // function OnClickYes() {
+    //     window.location.href = `/${surveyId}/selectProfile`;
+    // }
 
     function OnClickNo() {
         SetVariable(StorageVariable.SELECTED_PROFILE, "NoProfile");
@@ -41,27 +41,28 @@ function AskConsent() {
                     <SurveyTitle title={survey.Title}></SurveyTitle>
 
                     <main>
-                        <h2>¿Permitir que se recopilen las respuestas?</h2>
+                        <h2>Política de Privacidad</h2>
                         <p>
-                            Antes de comenzar la encuesta, nos gustaría pedir su consentimiento
-                            para recopilar y utilizar sus respuestas. Si acepta, le haremos algunas
-                            preguntas personales antes de comenzar y utilizaremos sus datos y
-                            respuestas para mejorar la calidad de la encuesta y la precisión de los resultados.
-                            La participación es voluntaria y sus respuestas serán tratadas con confidencialidad.
+                            Apreciamos su participación en nuestra encuesta y queremos asegurarle que la privacidad
+                            y la seguridad de sus respuestas son nuestra máxima prioridad. Todas sus respuestas serán
+                            completamente anónimas y no las recolectamos de ninguna manera.
                         </p>
 
                         <p>
-                            Por el momento la opción de recopilar datos no está disponible.
+                            Gracias por su colaboración.
                         </p>
 
                         <Container className="yesno-container">
                             <Row>
-                                <Col>
-                                    <Button className="yesno-btn me-4" onClick={OnClickYes} disabled variant="secondary">Sí</Button>
-                                </Col>
+                                {/* 
+                                Commented for the moment, since we don't store data yet
 
                                 <Col>
-                                    <Button className="yesno-btn" onClick={OnClickNo} variant="danger">No</Button>
+                                    <Button className="yesno-btn me-4" onClick={OnClickYes} disabled variant="secondary">Sí</Button>
+                                </Col> */}
+
+                                <Col>
+                                    <Button className="yesno-btn" onClick={OnClickNo} variant="secondary">Continuar</Button>
                                 </Col>
                             </Row>
                         </Container>
